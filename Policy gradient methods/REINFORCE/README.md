eeeece105076afb9942c30d72e2a370317527309
Jayesh Kandpal <jayeshkandpal@gmail.com>
2020-08-31 23:48:32 +0530

    minor changes

---
+++ Policy gradient methods/REINFORCE/README.md
@@ -0,0 +1,11 @@
+# REINFORCE 
+This is my implementation of the classi Monte carlo policy gradient algorithm also known as REINFORCE.
+
+## Results
+
+### Cartpole-v0
+Trained for 800 episodes : 
+![image](better one.png)
+
+### MountainCar-v0
+Trained for 4000 episodes :
---
+++ Policy gradient methods/REINFORCE/REINFORCE1.py
@@ -0,0 +1,59 @@
+import torch
+import torch.nn as nn
+from model import Policynetwork
+from utils import generate_episode, plot
+import numpy as np
+import gym
+
+env = gym.make('MountainCar-v0')
+
+state_space = env.observation_space.shape[0]
+action_space = env.action_space.n
+
+alpha = 0.001
+policy_network = Policynetwork(state_space, action_space)
+optimizer = torch.optim.Adam(policy_network.parameters(), lr = alpha)
+
+## TRAIN
+
+episodes = 4000
+rewards = []
+
+for episode in range(episodes):
+    
+    episode_experience, reward_list = generate_episode(env, policy_network)          # RETURNS (S,A,R,S') TUPLES OF THE EPISODE AND LIST OF REWARDS OBTAINED AT EACH STEP
+    total_reward = np.sum(reward_list)                  # TOTAL REWARD OF THE EPISODE
+    rewards.append(total_reward)                        # STORING TOTAL REWARD OF EACH EPISODE
+    loss = 0
+
+    for i,sars in enumerate(episode_experience):
+        
+        state, action, _, nextstate = sars
+        target_reward = np.sum(reward_list[i:-1]) - np.mean(reward_list)
+        reward_weight = torch.tensor(target_reward)                             # using monte carlo estimate for target, have i incorporated causality here??
+        action_logprob = -torch.log(policy_network(state))[action]
+        loss += action_logprob*reward_weight
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    if (episode+1)%400 == 0:
+      print(f'{episode+1}th episode; average reward of past 100 episodes :', np.mean(rewards[-100:]))              # PRINTING AVG OF LAST 100 EPISODES
+
+
+plot(rewards)
+
+# TESTING THE NETWORK
+
+done = False
+state = env.reset()
+policy_network.eval()
+while not done:
+	env.render()
+	action_probs = policy_network(state)
+	action = int(np.random.choice(np.arange(action_space), p=action_probs.detach().numpy()))
+	nextstate, reward, done, _ = env.step(action)
+	if done:
+		break
+	state = nextstate
+
